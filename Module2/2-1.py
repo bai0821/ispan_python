399b0f92ea6c489b33872d6c49a839a7c6168884
bai0821 <epicpug821@gmail.com>
2025-10-22 15:35:58 +0800

    own practice

---
+++ Module2/2-1.py
@@ -0,0 +1,19 @@
+# 建立第一支 Python 程式
+
+# 宣告變數
+x = 1 
+name = "William"
+
+
+
+# 輸出變數
+print(x)
+print(name)
+
+# Crtl + s 存檔
+# Crtl + / 註解程式碼
+# Crtl + ` 開啟終端機
+# Crtl + <numb> 回到上方視窗
+
+# Crtl + Z 修改上一步
+# Crtl + Y 返回修改
